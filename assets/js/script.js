1ac0376a1a4abaa2a4c316ff75872531be9b3a5a
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2021-08-01 21:50:13 -0300

    Coloquei os arquivos na pasta

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+//I USED JQUERY TO SMOOTH PAGE SCROLL
+$(function(){
+    $('li a').click(function(event) {
+        event.preventDefault(); 
+        var href = $(this).attr('href'); 
+        var offSetTop = $(href).offset().top; 
+        $('html, body').animate({ 
+            'scrollTop': offSetTop 
+        }, 'slow'); 
+    });
+});
+
+//
+var email = document.getElementById('email')
+email.addEventListener('blur', function() {
+    if(email.value == ""){
+        email.style.borderColor = 'hsl(0, 100%, 63%)'
+        msgerror.style.color = 'hsl(0, 100%, 63%)'
+        msgerror.style.display = 'block'
+        msgerror.innerHTML = 'Please enter a valid email address'
+    }else if(!email.checkValidity()){
+        email.style.borderColor = 'hsl(0, 100%, 63%)'
+        msgerror.style.color = 'hsl(0, 100%, 63%)'
+        msgerror.style.display = 'block'
+        msgerror.innerHTML = 'Please enter a valid email address'
+    }else{
+        msgerror.style.display = 'none'
+        email.style.borderColor = 'rgb(118, 118, 118)'
+    }
+})
+
+var msgerror = document.getElementById('msg-error')
+
+var btn = document.querySelector('.btn-form')
+btn.addEventListener('click', function() {
+    if(email.value == ""){
+        email.style.borderColor = 'hsl(0, 100%, 63%)'
+        msgerror.style.color = 'hsl(0, 100%, 63%)'
+        msgerror.style.display = 'block'
+        msgerror.innerHTML = 'Please enter a valid email address'
+    }else if(!email.checkValidity()){
+        email.style.borderColor = 'hsl(0, 100%, 63%)'
+        msgerror.style.color = 'hsl(0, 100%, 63%)'
+        msgerror.style.display = 'block'
+        msgerror.innerHTML = 'Please enter a valid email address'
+    }else{
+        msgerror.style.display = 'block'
+        email.style.borderColor = 'hsl(130, 55%, 57%)'
+        msgerror.style.color = 'hsl(130, 55%, 57%)'
+        msgerror.innerHTML = 'Valide Email'
+    }
+
+})
+
